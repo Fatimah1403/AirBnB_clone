9bb97e54af296340a2d8ab86eda4009bd317af8b
Naftali-B <nbudamba@gmail.com>
2023-05-09 08:28:10 -0700

    new task added

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    Base model that defines all common
+    attributes/methods for other classes
+"""
+
+class BaseModel:
+    """
+    Super class for the models
+    """
