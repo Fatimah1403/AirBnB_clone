bd44dccc42245e364175f1a52a04799341316d39
Naftali-B <nbudamba@gmail.com>
2023-05-10 07:52:15 -0700

    models and init file of the models folder

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+    """
+        Initializes the package and storage system for
+        Reloads FileStorage contents
+        Loads any existing data from disk into memory using reload()
+    """
+
+
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
+
--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,72 @@
 #!/usr/bin/python3
+"""
+    Base model that others inherit
+"""
 
-"""
-    Base model that defines all common
-    attributes/methods for other classes
-"""
+import uuid
+from datetime import datetime
+from models import storage
+
 
 class BaseModel:
+
     """
-    Super class for the models
+    All other classes will inherit this
     """
 
+    def __init__(self, *args, **kwargs):
+        
+        """
+        Initializing instance attributes
+        *args: varying number of arguments
+        **kwargs: key-values arguments; dictionary
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
     def __str__(self):
+        """
+        Instance of a class represented as a string
+        Returns a string with the format
+        [<class name>] (<id>) <instance attributes>.
+        """
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime
+        Calls the save() method of the storage module to save the instance
+        """
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """
+        Returns the my_dict dictionary containing all the keys and values of the instance
+        as well as the __class__, created_at, and updated_at keys
+        This dictionary will be used to serialize the instance into a JSON representation
+        """
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
+
