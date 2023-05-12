a897853ab5a7183afffebcf33ba2d11ea458f5cc
Fatimah1403 <fattyhassan14@gmail.com>
2023-05-12 22:08:57 +0000

    User test file updated

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -89,37 +89,109 @@ class Testuser(unittest.TestCase):
         self.assertNotIn(None, us_dict_val)
 
 
+class TestUser_save(unittest.TestCase):
+    """Unittests for testing save method of the  class."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_oneuser_save(self):
+        """ save only one user details"""
+        us = User()
+        sleep(0.08)
+        first_updat_at = us.updated_at
+        us.save()
+        self.assertLess(first_updat_at, us.updated_at)
+
+    def test_multipleuser_save(self):
+        """save multiple users details """
+        us = User()
+        sleep(0.08)
+        first_updat_at = us.updated_at
+        us.save()
+        second_updat_at = us.updated_at
+        self.assertLess(first_updat_at, us.updated_at)
+        sleep(0.08)
+        us.save()
+        self.assertLess(second_updat_at, us.updated_at)
+
+    def test_save_arg(self):
+        """Save arguments """
+        us = User()
+        with self.assertRaises(TypeError):
+            us.save(None)
+
+    def test_save_updated_file(self):
+        """Testing save updated files """
+        us = User()
+        us.save()
+        us_id = "User." + us.id
+        with open("storage.json", "r") as f:
+            self.assertIn(us_id, f.read())
 
 
+class TestUser_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the User class."""
 
+    def test_to_dict_type(self):
+        self.assertTrue(dict, type(User().to_dict()))
 
+    def test_to_dict_contains_correct_keys(self):
+        us = User()
+        self.assertIn("id", us.to_dict())
+        self.assertIn("created_at", us.to_dict())
+        self.assertIn("updated_at", us.to_dict())
+        self.assertIn("__class__", us.to_dict())
 
+    def test_to_dict_contains_added_attributes(self):
+        us = User()
+        us.middle_name = "Kehinde"
+        us.number = 40
+        self.assertEqual("Kehinde", us.middle_name)
+        self.assertIn("number", us.to_dict())
 
+    def test_to_dict_datetime_attributes_are_strs(self):
+        us = User()
+        us_dict = us.to_dict()
+        self.assertEqual(str, type(us_dict["id"]))
+        self.assertEqual(str, type(us_dict["created_at"]))
+        self.assertEqual(str, type(us_dict["updated_at"]))
 
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        us = User()
+        us.id = "123456"
+        us.created_at = us.updated_at = dt
+        tdict = {
+            'id': '123456',
+            '__class__': 'User',
+            'created_at': dt.isoformat(),
+            'updated_at': dt.isoformat(),
+        }
+        self.assertDictEqual(us.to_dict(), tdict)
 
+    def test_contrast_to_dict_dunder_dict(self):
+        us = User()
+        self.assertNotEqual(us.to_dict(), us.__dict__)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_to_dict_with_arg(self):
+        us = User()
+        with self.assertRaises(TypeError):
+            us.to_dict(None)
 
 
 if __name__ == "__main__":
