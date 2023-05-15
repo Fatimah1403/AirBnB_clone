5a3c3e91b61e36e28cf5c8cb1bcb6ef506c5c98b
Naftali Budamba <nbudamba@gmail.com>
2023-05-15 04:41:23 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Amenity class that inherits from BaseModel"""
 
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """ Amenity class
